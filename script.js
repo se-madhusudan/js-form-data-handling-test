1adea3fac3ab3e2e4b120dc473013c8d6c5e7cd8
Dharminder7 <madhusudan.kumar@tmotions.com>
2025-01-31 15:33:49 +0530

    pop up ui created

---
+++ script.js
@@ -0,0 +1,29 @@
+const detailsForm = document.querySelector('#detailsForm');
+
+
+
+
+//function to validate the form data and show errors, show submit pop up post data validation, calls submitData()
+const validateData = () => {
+
+}
+
+//function to parse data into json and save in the local storage, hides the submit pop up
+const submitData = () => {
+
+}
+
+//function to fetch data from local storage and view data, shows view pop up
+const viewData = () => {
+
+}
+
+//function to pre-fill the form data in form after fetching from local storage, calls validateData()
+const editData = () => {
+
+}
+
+//function to fetch the name, email from the local storage and show in table row to allow viewing and editing all data
+const tableData = () => {
+
+}
